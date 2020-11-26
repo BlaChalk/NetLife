e747eb97621d868b58055483b3f255ce37295f3a
BlaChalk <black00731@gmail.com>
2020-11-26 23:03:22 +0800

    + index@view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>NetLife</title>
+    <link rel="stylesheet" href="/css/frontend.css">
+</head>
+<body>
+    <img class="logo" src="/img/logo.svg" alt="logo">
+    <div class="banner">
+        <img id="people1" src="/img/people.svg" alt="people">
+        <img id="people2" src="/img/people.svg" alt="people">
+        <img id="people3" src="/img/people.svg" alt="people">
+        <img id="people4" src="/img/people.svg" alt="people">
+        <img id="people5" src="/img/people.svg" alt="people">
+        <img id="people6" src="/img/people.svg" alt="people">
+        <img class="notFound" src="/img/404.svg" alt="404">
+    </div>
+</body>
+</html>
