ef3fab722f638b288d01f791481b93f062a40887
BlaChalk <black00731@gmail.com>
2020-11-27 14:11:54 +0800

    update index@view + title & text

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3Jlc291cmNlcy9qcy9mcm9udGVuZC5qcy5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./resources/js/frontend.js\n");
 
 /***/ }),
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,19 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>NetLife</title>
+    <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/frontend.css">
 </head>
+<script src="/js/app.js"></script>
 <body>
-    <img class="logo" src="/img/logo.svg" alt="logo">
-    <div class="banner">
-        <img class="noise" src="/img/noise.svg" alt="">
-        <img id="people1" src="/img/people.svg" alt="people">
-        <img id="people2" src="/img/people.svg" alt="people">
-        <img id="people3" src="/img/people.svg" alt="people">
-        <img id="people4" src="/img/people.svg" alt="people">
-        <img id="people5" src="/img/people.svg" alt="people">
-        <img id="people6" src="/img/people.svg" alt="people">
-        <img class="notFound" src="/img/404.svg" alt="404">
+    <div class="container fill">
+        <img class="logo" src="/img/logo.svg" alt="logo">
+        <div class="banner">
+            <img class="noise" src="/img/noise.svg" alt="">
+            <img id="people1" src="/img/people.svg" alt="people">
+            <img id="people2" src="/img/people.svg" alt="people">
+            <img id="people3" src="/img/people.svg" alt="people">
+            <img id="people4" src="/img/people.svg" alt="people">
+            <img id="people5" src="/img/people.svg" alt="people">
+            <img id="people6" src="/img/people.svg" alt="people">
+            <img class="notFound" src="/img/404.svg" alt="404">
+        </div>
+    </div>
+    <div class="container fill">
+        <div class="row title">
+            <div class="col">
+                <h2>您知道什麼是「數位成癮」嗎？
+                </h2>
+            </div>
+        </div>
+        <div class="row text">
+            <div class="col">
+                <h4>數位成癮，泛指過度使用數位內容相關的實體或非實體的產品，如：手機、平板、電腦、虛擬體感遊戲、智慧手錶、智慧助理…等。當使用者過度使用上述產品，而導致身心健康、個人學業、人際關係、家庭互動或工作表現等出現負面影響。這種不健康的使用習慣都稱之為「數位成癮」。</h4>
+            </div>
+        </div>
     </div>
 </body>
 </html>
