288a6ac352f32c1079702a3b65deb05fdbd3040f
BlaChalk <black00731@gmail.com>
2020-11-27 16:57:24 +0800

    update index@view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,31 +10,143 @@
 </head>
 <script src="/js/app.js"></script>
 <body>
+    <div class="container h-100">
+        <div class="row h-100">
+            <img class="logo" src="/img/logo.svg" alt="logo">
+            <div class="banner">
+                <img class="noise" src="/img/noise.svg" alt="">
+                <img id="people1" src="/img/people.svg" alt="people">
+                <img id="people2" src="/img/people.svg" alt="people">
+                <img id="people3" src="/img/people.svg" alt="people">
+                <img id="people4" src="/img/people.svg" alt="people">
+                <img id="people5" src="/img/people.svg" alt="people">
+                <img id="people6" src="/img/people.svg" alt="people">
+                <img class="notFound" src="/img/404.svg" alt="404">
+            </div>
+        </div>
+        <div class="row h-100">
+            <div class="col-sm-9 col-12">
+                <div id="section1">
+                    <div id="title-1" class="title-bg h-2">
+                        <h2 class="title">您知道什麼是「數位成癮」嗎？</h2>
+                    </div>
+                    <h4 class="text">數位成癮，泛指過度使用數位內容相關的實體或非實體的產品，如：手機、平板、電腦、虛擬體感遊戲、智慧手錶、智慧助理…等。當使用者過度使用上述產品，而導致身心健康、個人學業、人際關係、家庭互動或工作表現等出現負面影響。這種不健康的使用習慣都稱之為「數位成癮」。</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row h-100">
+            <div class="effect">
+            </div>
+            <div class="col-sm-9 col-12">
+                <div id="section2">
+                    <div id="title-2" class="title-bg h-2">
+                        <h2 class="title">「數位成癮」常見型態有哪些呢？</h2>
+                    </div>
+                    <h4 class="text">每個人使用數位產品的目的和用途不盡相同，因此造就使用者染上不同型態的數位成癮。例如：網路遊戲成癮、色情網站成癮、社群網站成癮、影音網站成癮、網路文學網站成癮、漫畫網站成癮等。其中又以『網路遊戲成癮』最常見。</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row h-100">
+            <div class="col-sm-12 col-12">
+                <div id="section3">
+                    <div id="title-3" class="title-bg h-2 title-red">
+                        <h2 class="title">「數位成癮」是一種病嗎？</h2>
+                    </div>
+                    <h4 id="text-3" class="text">基本上「數位成癮」就跟所有成癮行為一樣，可能是生活壓力造成也可能是人際關係造成，成癮原因百百種。「數位成癮」通常會出現以下三種情況：<br>
+                        1.整天一直想<br>
+                        2.上手停不了<br>
+                        3.生活亂糟糟<br>
+                        若上述三種情況已經持續超過三個月以上，<br>
+                        就需要考慮去醫院尋求協助醫師的專業協助。</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- <div class="container fill">
+
+    </div>
+
     <div class="container fill">
-        <img class="logo" src="/img/logo.svg" alt="logo">
-        <div class="banner">
-            <img class="noise" src="/img/noise.svg" alt="">
-            <img id="people1" src="/img/people.svg" alt="people">
-            <img id="people2" src="/img/people.svg" alt="people">
-            <img id="people3" src="/img/people.svg" alt="people">
-            <img id="people4" src="/img/people.svg" alt="people">
-            <img id="people5" src="/img/people.svg" alt="people">
-            <img id="people6" src="/img/people.svg" alt="people">
-            <img class="notFound" src="/img/404.svg" alt="404">
+        <div class="row">
+            <div class="col-sm-9 col-8">
+                <div id="section4">
+                    <h4 class="text">目前僅有網路遊戲成癮被世界衛生組織在《國際疾病分 類》（ICD-11）正式列為精神疾病：「遊戲障礙症」（Gaming disorder），定義為一種遊戲行為模式（＂網路或電視遊戲＂）。要診斷出遊戲障礙，其行為必須具有嚴重性，導致個人、家庭社會、教育、職業或其他重要功能領域重大損害。通常至少會持續12個月。</h4>
+                    <p>遊戲障礙症的特徵為：<br>
+                        1.	玩遊戲的控制力受損<br>
+                        2.	生活以電玩為優先，忽略其他興趣和日常活動。<br>
+                        3.	即使造成負面影響，仍持續增加打電玩時間。
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
     <div class="container fill">
-        <div class="row title">
-            <div class="col">
-                <h2>您知道什麼是「數位成癮」嗎？
-                </h2>
-            </div>
-        </div>
-        <div class="row text">
-            <div class="col">
-                <h4>數位成癮，泛指過度使用數位內容相關的實體或非實體的產品，如：手機、平板、電腦、虛擬體感遊戲、智慧手錶、智慧助理…等。當使用者過度使用上述產品，而導致身心健康、個人學業、人際關係、家庭互動或工作表現等出現負面影響。這種不健康的使用習慣都稱之為「數位成癮」。</h4>
+        <div class="row">
+            <div class="col-sm-9 col-8">
+                <div id="section5">
+                    <h4 class="text">「數位成癮」會造成什麼影響呢？
+                        每個人使用數位產品的目的和用途不一樣，因此成癮的原因也不盡相同，數位成癮帶來的影響大致可以分為層面身理和心理層面，例如：有些人因工作不得不長時間依賴手機，導致自己不時時刻刻關注通知就會渾身不舒服；有些人在現實中得不到關愛，而將情感投注在虛擬世界的交流。其中又以視力衰退、生活作息錯亂、無法專注、社交障礙、家庭溝通不良最為常見。
+                    </h4>
+                </div>
             </div>
         </div>
     </div>
+    <div class="container fill">
+        <div class="row">
+            <div class="col-sm-9 col-8">
+                <div id="section6">
+                    <h4 class="text">「數位成癮」該如何預防呢？
+                        預防數位成癮最好的方法是以個人為出發點，自行檢視自己的使用習慣，自行找到適合自己的使用模式。而預防的方法依序分為四個步驟：觀察、分析、規劃、執行。
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container fill">
+        <div class="row">
+            <div class="col-sm-9 col-8">
+                <div id="section7">
+                    <h2 class="title">關於我們</h2>
+                    <h4 class="text">我就如同一艘小船行經在名為數位科技的水域上，水帶給我們方便，也能將我們覆滅。我們想利用網站有系統地呈現一套針對『數位成癮』的學習資源，以提升家長與孩子對數位成癮的認知，一起學習健康的使用習慣。讓家長放心孩子們使用數位科技，孩子擁有更豐富的學習資源。</h4>
+                    <h2 class="title">理念誕生</h2>
+                    <h4 class="text">在數位時代成長的我們，比父母輩擁有更多的資源可以自由學習，但數位的世界充滿豐富刺激的體驗，孩子們容易被受吸引，甚至養成不當的使用習慣。父母們常選擇極端的方式禁止孩子使用，經常導致嚴重的家庭問題，甚至讓孩子們失去一個學習的管道。我們希望透過資源整合的方式，提升家長與孩子對於數位成癮的認知，共同學習數位健康的的重要。</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container fill">
+        <div class="row">
+            <div class="col-sm-9 col-8">
+                <div id="section8">
+                    <h4 class="text">『奈特人生』的由來<br>
+                        『奈特』，意旨『internet/net』。<br>
+                        在國家政策的推動下，網路普及率逐年攀高，促使台灣趨向數位化社會發展。網路在日常生活中處處可見，我們一生幾乎脫離不了網路。如何在現實與虛擬之間取得平和，是你我都需要要重視的問題。
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container fill">
+        <div class="row">
+            <div class="col-12">
+                <div id="section9">
+                    <div class="title-bg">
+                        <h2 class="title">組員介紹</h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="/img/KY.svg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="/img/YL.svg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="/img/SY.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> --}}
 </body>
 </html>
